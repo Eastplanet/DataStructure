94547987299c3cee2093c2cfe30500bf01787284
Eastplanet <ehdrb700606@naver.com>
2021-02-26 23:45:53 +0900

    DataStructure-Stack

--- DataStructure.cpp
+++ DataStructure.cpp
@@ -1,6 +1,19 @@
 ﻿#include <iostream>
+#include "Stack.h"
 using namespace std;
 int main()
 {
-	
+	Stack stack(5);
+	stack.push('a');
+	stack.push('b');
+	stack.push('a');
+	stack.push('b');
+	stack.push('a');
+	stack.push('b');
+	stack.print();
+	stack.pop();
+	stack.pop();
+	stack.print();
+
+	return 0;
 }
