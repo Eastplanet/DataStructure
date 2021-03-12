cefb4ab7e3ad25763ea68232c937ec09d787cae0
Eastplanet <ehdrb700606@naver.com>
2021-03-12 16:58:25 +0900

    DataStructure-Floyd

--- DataStructure.cpp
+++ DataStructure.cpp
@@ -1,20 +1,39 @@
 ﻿#include <iostream>
-#include "CircularQueue.h"
+#include <cstring>
+#include "Floyd.h"
+#define INF 9999
 using namespace std;
 int main()
 {
-	CircularQueue que(5);
-	que.enqueue('a');
-	que.enqueue('b');
-	que.enqueue('c');
-	que.enqueue('d');
-	que.enqueue('e');
-	que.enqueue('f');
-	que.dequeue();
-	que.dequeue();
-	que.dequeue();
-	que.dequeue();
-	que.dequeue();
-	que.dequeue();
+	int** map = new int* [100];
+	for (int i = 0; i < 100; i++) {
+		map[i] = new int[100];
+		fill(map[i], map[i] + 100, INF);
+	}
+	
+	map[1][3] = 1;
+	map[3][2] = 4;
+	map[1][2] = 1000;
+
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			cout << map[i][j]<<" ";
+		}
+		cout << "\n";
+	}
+	map = Floyd(map);
+
+	cout << "\n";
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			cout << map[i][j]<<" ";
+		}
+		cout << "\n";
+	}
+
+
+
+	for (int i = 0; i < 100; i++)delete map[i];
+	delete map;
 	return 0;
 }
